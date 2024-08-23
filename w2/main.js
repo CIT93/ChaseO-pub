2ba1ec315c4f9ec73cc299c91a54ae639f46dad4
Chase Olisa <chaseolisa@gmail.com>
2024-08-22 23:52:50 -0700

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,23 @@
+// How to Calculate Your Carbon Footprint - https://www.wikihow.com/Calculate-Your-Carbon-Footprint
+// Method 1
+
+// 1. Calculating Your Carbon Footprint Points.
+const myHouse = 12;
+// 2. Consider the size of your home.
+const myHomeSize = 2;
+// 3. Evaluate your food choices.
+const myFoodChoices = 18;
+// 4. Examine your water consumption.
+const myWaterConsumption = 2;
+// 5. Determine how many household purchases you make each year.
+const myHouseholdItemsBought = 4;
+// 6. Consider how much waste you produce.
+const myWaste = 50;
+// 7. Identify the amount of waste that you recycle.
+const myRecycle = 6;
+// 8. Tally up your annual transportation scores.
+const myTransportScore = 4;
+// 9. Add up your points.
+const cfpTotal = myHouse + myHomeSize + myFoodChoices + myWaterConsumption + myHouseholdItemsBought + myWaste + myRecycle + myTransportScore
+// 10. Write JS to update the rendered html (index.html) with total footprint
+
