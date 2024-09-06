5be101cd10d245bb30fcfeaf4ec54aa16e19b21d
Chase Olisa <chaseolisa@gmail.com>
2024-09-06 02:49:29 -0700

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -31,7 +31,7 @@ function determineHouseholdPts(numberInHousehold) {
     houseHoldPoints = 6;
   } else if (numberInHousehold === 6) {
     houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
+  } else if (numberInHousehold > 7) {
     houseHoldPoints = 2;
   }
   
@@ -46,12 +46,45 @@ function start(houseHoldMembers, houseSize) {
 }
 
 function displayOutput() {
-  
+ for (arr of cfpData){
+  console.log(arr)
+  const output = document.getElementById("output");
+  const newP = document.createElement("p");
+  newP.textContent = `Carbon Footprint total is ${arr[4]}, you get ${arr[2]} points, if your members are ${arr[0]}, so your living arrangements would be ${arr[1]}, then your points should be ${arr[3]}.`;
+  output.appendChild(newP)
+ }
 }
 
 start(5, "apt");
 start(4, "large");
 start(3, "medium");
+start(1, "small");
+start(1, "apt");
+start(1, "medium");
+start(1, "large");
+start(2, "apt");
 start(2, "small");
+start(2, "medium");
+start(2, "large");
+start(3, "apt");
+start(3, "small");
+start(3, "medium");
+start(3, "large");
+start(4, "apt");
+start(4, "small");
+start(4, "medium");
+start(4, "large");
+start(5, "apt");
+start(5, "small");
+start(5, "medium");
+start(5, "large");
+start(6, "apt");
+start(6, "small");
+start(6, "medium");
+start(6, "large");
+start(7, "apt");
+start(7, "small");
+start(7, "medium");
+start(7, "large");
 
-displayOutput()
+displayOutput();
